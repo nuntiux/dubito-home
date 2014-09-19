0d9d06cbcce575e23b94c252d6954e246daf4520
Samuel Pasquier <samuel@happycoders.org>
2014-09-19 05:32:31 +0000

    * Starting the db daemon

--- scripts/dubito-home-lcd-daemon.py
+++ scripts/dubito-home-lcd-daemon.py
@@ -2,7 +2,7 @@
 # ----------------------------------------------------------------------------
 # Samuel Pasquier - samuel@happycoders.org
 # 
-# Dubito home  Automation
+# Dubito home Automation
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
