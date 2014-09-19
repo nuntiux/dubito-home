dc5cf4908b3c25638b7c0b9c275be7ee633c9e53
Samuel Pasquier <samuel@pasquier.name>
2014-09-18 21:35:35 -0700

    * update

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-test-pi
-=======
+Dubito-home
+===========
 
-test-pi
+This is my personal repository for a personal project to automate my home.
+I'm using this particular project to discover and play with python.
+
+Feel free to copy if you find anything interesting here.
+
--- init.d/dubito-home-lcd-daemon.sh
+++ init.d/dubito-home-lcd-daemon.sh
@@ -9,11 +9,27 @@
 ### END INIT INFO
  
 #! /bin/sh
+#
+# Samuel Pasquier - samuel@happycoders.org
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 # /etc/init.d/dubito-home-lcd-daemon
 
 set -e 
 
-export ROOTDIR=/root/test-pi/dubito-home/init.d
+export ROOTDIR=/root/dubito-home/init.d
 
 . /lib/lsb/init-functions
 
